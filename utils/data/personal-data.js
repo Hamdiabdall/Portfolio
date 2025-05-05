55e8b42b09cb0ab9dd58d50c06b4889743b1209f
hamdiabdall <hamdi.abdallah@polytechnicien.tn>
2025-05-05 22:00:01 +0100

    fix some errors

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Hamdi Abdallah",
-  profile: '/profile.png',
+  profile: '/Profile.png',
   designation: "Software Enginner",
   description: "My name is Hamdi Abdallah. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'hamdi.abdallah@polytechnicien.tn ',
