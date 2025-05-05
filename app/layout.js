2c5a4c05e61677684deb51d5565df5c16aa98130
hamdiabdall <hamdi.abdallah@polytechnicien.tn>
2025-05-05 21:03:06 +0100

    update project and fix some errors

--- app/layout.js
+++ app/layout.js
@@ -12,17 +12,29 @@ export const metadata = {
   title: "Portfolio of Hamdi Abdallah - Software Enginner",
   description:
     "This is the portfolio of Hamdi Abdallah. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  formatDetection: {
+    telephone: false,
+    date: false,
+    address: false,
+    email: false,
+  },
 };
+
+import ClientOnly from "./components/helper/client-only";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <ClientOnly>
+          <ToastContainer />
+        </ClientOnly>
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
-          <ScrollToTop />
+          <ClientOnly>
+            <ScrollToTop />
+          </ClientOnly>
         </main>
         <Footer />
       </body>
